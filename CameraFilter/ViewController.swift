304b10cb10307858d659ed302ef7dea9a68f1a42
Mariana <mariana@fastmail.net>
2022-01-05 16:35:27 +0100

    start project

---
+++ CameraFilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Mariana Steblii on 05/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
