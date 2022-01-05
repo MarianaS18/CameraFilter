0b266aaac0a5913f4584d40a5a5227c8d45b3638
Mariana <mariana@fastmail.net>
2022-01-05 19:04:16 +0100

    passing selected photo with Observable

--- CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/PhotosCollectionViewController.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 import Photos
+import RxSwift
 
 class PhotosCollectionViewController: UICollectionViewController {
+    private let selectedPhotoSubject = PublishSubject<UIImage>()
+    var selectedPhtot: Observable<UIImage> {
+        return selectedPhotoSubject.asObservable()
+    }
+    
     private var images = [PHAsset]()
     
     override func viewDidLoad() {
@@ -32,6 +38,7 @@ class PhotosCollectionViewController: UICollectionViewController {
         let asset = images[indexPath.row]
         // help to get the image
         let manager = PHImageManager.default()
+        
         manager.requestImage(for: asset, targetSize: CGSize(width: 120, height: 120), contentMode: .aspectFill, options: nil) { image, _ in
             DispatchQueue.main.async {
                 cell.photoImageView.image = image
@@ -39,6 +46,25 @@ class PhotosCollectionViewController: UICollectionViewController {
         }
         
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectAsset = images[indexPath.row]
+        
+        PHImageManager.default().requestImage(for: selectAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self] image, info in
+
+            // requestImage is called two times: first time it gives thumbnail image, and the second time it gives the much bigger image. The key "PHImageResultIsDegradedKey" says if it thumbnail or original image
+            guard let info = info else { return }
+            let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+            
+            // now we get the original image
+            if !isDegradedImage {
+                if let image = image {
+                    self?.selectedPhotoSubject.onNext(image)
+                    self?.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     private func populatePhotos() {
