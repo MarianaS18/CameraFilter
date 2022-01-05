fd4a330c3bee759623fd64cf27c20607822d92ea
Mariana <mariana@fastmail.net>
2022-01-05 17:49:27 +0100

    setup UI

---
+++ CameraFilter/PhotosCollectionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosCollectionViewController.swift
+//  CameraFilter
+//
+//  Created by Mariana Steblii on 05/01/2022.
+//
+
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+}
